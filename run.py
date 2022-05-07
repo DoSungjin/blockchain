f559a50c183ce12f9ff05d054376c07818ac598b
sungjindo <tjdwls6366@gmail.com>
2022-05-08 02:30:46 +0900

    third commit

--- run.py
+++ run.py
@@ -3,3 +3,13 @@ print('python2')
 print('python3')
 # 추가하는것 
 print("start")
+# $ git init 처음할때 하는것
+# $ git add .
+# $ git status
+# $ git commit -m "first commit" 이름정하기
+# $ git remote add origin https://github.com/sungjindo/test-project.git 연동시키기
+# $ git remote -v 만들기
+# $ git push origin master 파일 올리기
+# $ git add. 추가사항 만들기 $ git status
+# $ git commit -m "second commit" 이름 변경
+# $ git push origin master 추가사항 올리기 
